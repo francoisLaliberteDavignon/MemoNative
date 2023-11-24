11154a3333f4514de2474d50da48179048a7f27f
francoisLaliberteDavignon <flaliberte@live.ca>
2023-11-24 10:27:30 -0500

    init + header

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from './components/LoginPage';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <LoginPage/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aaa',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
